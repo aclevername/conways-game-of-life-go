9667e907b9a4c9471b29fcc2c9c71cf8a8ca8f86
jake <jakelarsj@gmail.com>
2018-07-11 14:37:17 +0100

    Finish off update grid and implement print function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -18,7 +19,6 @@ func main() {
 	size := len(lines[0])
 
 	currentGrid := makeGrid(size)
-	futureGrid := makeGrid(size)
 
 	for y, line := range lines {
 		for x, c := range line {
@@ -26,9 +26,79 @@ func main() {
 			if c == '1' {
 				value = 1
 			}
-			fmt.Print(value)
 			currentGrid[y][x] = value
 		}
+	}
+	printGrid(currentGrid)
+	for {
+		updateGrid(currentGrid)
+		printGrid(currentGrid)
+		time.Sleep(time.Second / 10)
+	}
+}
+
+func printGrid(grid [][]int) {
+	fmt.Println()
+	for _, row := range grid {
+		for _, value := range row {
+			if value == 0 {
+				fmt.Print("-")
+			} else {
+				fmt.Print("o")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func updateGrid(grid [][]int) {
+	futureGrid := makeGrid(len(grid))
+	for y, row := range grid {
+		for x, _ := range row {
+			futureGrid[y][x] = getValue(grid, x, y)
+		}
+	}
+
+	for y, row := range grid {
+		for x, _ := range row {
+			grid[y][x] = futureGrid[y][x]
+		}
+	}
+}
+
+type position struct {
+	x int
+	y int
+}
+
+func getValue(grid [][]int, x, y int) int {
+	cells := []position{
+		position{x + 1, y - 1}, position{x + 1, y}, position{x + 1, y + 1},
+		position{x, y - 1}, position{x, y + 1},
+		position{x - 1, y - 1}, position{x - 1, y}, position{x - 1, y + 1},
+	}
+
+	count := 0
+	size := len(grid)
+
+	for _, cell := range cells {
+		x := cell.x
+		y := cell.y
+		if x >= 0 && x < size && y >= 0 && y < size && grid[cell.y][cell.x] == 1 {
+			count++
+		}
+	}
+
+	if grid[y][x] == 1 {
+		if count == 2 || count == 3 {
+			return 1
+		}
+	} else {
+		if count == 3 {
+			return 1
+		}
+	}
+	return 0
 }
 
 func makeGrid(size int) [][]int {
